1f297407e5af222bfccb844bb99ee0553178620f
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-09-04 01:12:16 +0900

    added signal receiver

--- main_server.c
+++ main_server.c
@@ -1,18 +1,26 @@
 #include "minitalk.h"
 
-void pid_print(void)
+void	pid_print(void)
 {
-	char *pid_s;
+	char	*pid_s;
 
 	pid_s = ft_itoa(getpid());
-	write(1, "pid = ", 6);
+	write(1, "SERVER PID_(:3 <)_ : ", 6);
 	ft_putendl_fd(pid_s, 1);
 	free(pid_s);
 }
 
-// #ifndef TEST
-// int main ()
-// {
-// 	pid_print();
-// }
-// #endif
+void	sig2bit(void)
+{
+}
+
+int	main(void)
+{
+	pid_print();
+	while (1)
+	{
+		signal(SIGUSR2, sig2bit);
+		signal(SIGUSR1, sig2bit);
+		pause();
+	}
+}
