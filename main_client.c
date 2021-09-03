2e9ada3072faac2bbe648b6f00e012d7edba724e
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-09-04 01:10:17 +0900

    added send message module

--- main_client.c
+++ main_client.c
@@ -1,5 +1,9 @@
 #include "minitalk.h"
 
+/*
+**
+*/
+
 void write_error(char *s)
 {
 	ft_putendl_fd(s, 1);
@@ -10,21 +14,27 @@ void	pid_handler(char *s_pid, char *s)
 {
 	int	pid;
 	int	i;
+	uint8_t	counter;
 
 	pid = ft_atoi(s_pid);
 	i = 0;
+	/*1<<6 = 1000000(2) = 64(10)*/
+	counter = 1 << 6;
 	while(i <= ft_strlen(s))
 	{
+		while()
+		{
+
+		}
 		i++;
 	}
-
-
 }
 
 int main(int argc, char **argv)
 {
 	if (argc != 3)
-		write_error("COMMAND: ./client [server-pid] [message]\n");
-	signal(SIGUSR1, &write_error);
-	
+		write_error("COMMAND( !_!): ./client [server-pid] [message]\n");
+	ft_putendl_fd("Success :-)\n", 1);
+	pid_handler(argv[1], argv[2]);
+	return (0);
 }
