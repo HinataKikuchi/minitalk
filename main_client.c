4bdcd16373f13c7ced97e9ceaf3b2f04a739ce4e
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-09-18 19:32:35 +0900

    added references

--- main_client.c
+++ main_client.c
@@ -1,7 +1,7 @@
 #include "minitalk.h"
 
 /*
-**
+**SEE THIS! https://github.com/mznmk/minitalk/wiki/what_to_do
 */
 
 void	write_error(char *s)
@@ -24,12 +24,12 @@ static void	pid_handler(char *s_pid, char *s)
 			if (*s & counter)
 			{
 				if (kill(pid, SIGUSR1) == -1)
-					write_error("BAD PID( !_!)\n");
+					write_error("BAD PID( !_!)");
 			}
 			else
 			{
 				if (kill(pid, SIGUSR2) == -1)
-					write_error("BAD PID( !_!)\n");
+					write_error("BAD PID( !_!)");
 			}
 			counter >>= 1;
 			usleep(600);
@@ -41,8 +41,8 @@ static void	pid_handler(char *s_pid, char *s)
 int	main(int argc, char **argv)
 {
 	if (argc != 3)
-		write_error("COMMAND( !_!): ./client [server-pid] [message]\n");
-	ft_putendl_fd("Success :-)\n", 1);
+		write_error("COMMAND( !_!): ./client [server-pid] [message]");
+	// ft_putendl_fd("Success :-)\n", 1);
 	pid_handler(argv[1], argv[2]);
 	return (0);
 }
