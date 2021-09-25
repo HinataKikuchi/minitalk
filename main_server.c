0217c102171f8e6e28565251890617d9b528ba3e
hkikuchi <hkikuchi@student.42tokyo.jp>
2021-09-25 11:46:24 +0900

    simple is all u need lol

--- main_server.c
+++ main_server.c
@@ -4,42 +4,25 @@ static t_message msg;
 
 void	pid_print(void)
 {
-	char	*pid_s;
-
-	pid_s = ft_itoa(getpid());
-	write(1, "SERVER PID_(:3 <)_ : ", 6);
-	ft_putendl_fd(pid_s, 1);
-	free(pid_s);
+	ft_putstr_fd("SERVER PID_(:3 <)_ : ", 1);
+	ft_putnbr_fd(getpid(),1);
 }
 
-static void	recieve_sig1(int b)
+static void	sig_handler(int a)
 {
-	// msg.c += ();
-	printf("SIGUSR1\n");
-	// exit(0);
-}
-
-static void	recieve_sig2(int a)
-{
-	printf("SIGUSR2\n");
-	// exit(0);
+	if (a == SIGUSR1)
+		printf("recieved SIGUSR1\n");
+	else if(a == SIGUSR2)
+		printf("recieved SIGUSR2\n");
 }
 
 int	main(void)
 {
-	struct sigaction	sig1;
-	struct sigaction	sig2;
+	struct sigaction	sig;
 	pid_print();
-	msg.c = 0;
-	msg.s = 0;
-	sig1.sa_handler = &recieve_sig1;
-	sig2.sa_handler = &recieve_sig2;
-	sigaction(SIGUSR1, &sig1, NULL);
-	sigaction(SIGUSR2, &sig2, NULL);
-	// if (sigaction(SIGUSR1, &sig1, NULL) == -1)
-	// 	write_error("( !_!) FAILED SIGACTION1");
-	// if (sigaction(SIGUSR2, &sig2, NULL))
-	// 	write_error("( !_!) FAILED SIGACTION2");
+	sig.sa_handler = &sig_handler;
+	sigaction(SIGUSR1, &sig, NULL);
+	sigaction(SIGUSR2, &sig, NULL);
 	while (1)
 		pause();
 }
